65e4f74601762cf69ebd657a72caee6652060625
Yogesh Sharma <yogesh.sharma.007321@gmail.com>
2021-09-10 23:54:52 +0530

    description length increased

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
     has_many :article_categories
     has_many :categories, through: :article_categories
     validates :title, presence: true, length: {minimum: 6, maximum: 30}
-    validates :description, presence: true, length: {minimum: 10, maximum: 200}
+    validates :description, presence: true, length: {minimum: 10, maximum: 500}
     
 end
